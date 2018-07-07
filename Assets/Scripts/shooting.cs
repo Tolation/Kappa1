e1c128c1c32c7806386f777668fd41a1b98a1f7b
tolis <tolis.bov@hotmail.com>
2018-07-07 12:24:01 +0300

    07

---
+++ Assets/Scripts/shooting.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shooting : MonoBehaviour {
+	public GameObject bullet;
+	public GameObject bulletspawn;
+	public float speed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetMouseButtonDown(0)){
+			GameObject s = Instantiate(bullet,bulletspawn.transform.position,bulletspawn.transform.rotation);
+			s.GetComponent<Rigidbody>().velocity=-s.transform.forward*speed;
+			s.transform.LookAt(s.transform.forward);
+			Destroy(s,2);
+		}
+		
+	}
+}
